da68b5ae99dc356eb55e0386cf0b60c3a8abab4c
Alvaro Acevedo <acevedo.alvaro@gmail.com>
2020-03-22 19:40:21 +0000

    first commit

--- src/test/java/com/translator/DocumentTranslatorTest.java
+++ src/test/java/com/translator/DocumentTranslatorTest.java
@@ -7,13 +7,13 @@ class DocumentTranslatorTest {
 
 	@Ignore
 	void trasnlatetoFRtest() {
-		String[] args = {"-filePath", "./src/test/resources/docs/asylum.gov.uk.pdf", "-targetLanguage", "fr"} ;
+		String[] args = {"-filePath", "./src/test/resources/docs/top7-trends-mule-soft.pdf", "-targetLanguage", "fr"} ;
 		DocumentTranslator.main(args);
 	}
 	
 	@Test
 	void trasnlatetoEStest() {
-		String[] args = {"-filePath", "./src/test/resources/docs/asylum.gov.uk.pdf", "-targetLanguage", "es"} ;
+		String[] args = {"-filePath", "./src/test/resources/docs/top7-trends-mule-soft.pdf", "-targetLanguage", "es"} ;
 		DocumentTranslator.main(args);
 	}
 
