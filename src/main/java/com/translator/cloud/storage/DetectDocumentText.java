de621ba18c129a1018ffe36e245f6787863bcec8
Alvaro Acevedo <acevedo.alvaro@gmail.com>
2020-04-25 10:44:07 +0100

    fix typo in unit tests, add new document and test, and main method in the DetectDocumentText

--- src/main/java/com/translator/cloud/storage/DetectDocumentText.java
+++ src/main/java/com/translator/cloud/storage/DetectDocumentText.java
@@ -18,11 +18,20 @@ package com.translator.cloud.storage;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,6 +62,46 @@ import com.translator.tools.Utils;
 public class DetectDocumentText {
 
 	private static final Logger log = LoggerFactory.getLogger(DetectDocumentText.class);
+	
+	public static void main(String[] args) {
+		final Options options = new Options();
+		options.addOption(Option.builder("filePath").argName("filePath").hasArg().desc("Path to the file to translate.").required().build());
+		options.addOption(Option.builder("configPath").argName("configPath").hasArg().desc("Path to the configuration file describing Google Cloud project Id and Bucket Name").required(false).build());
+		
+		CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+		try {
+			cmd = parser.parse(options, args);
+
+			String filePath = cmd.getOptionValue("filePath");
+			String configPath = cmd.getOptionValue("configPath");
+			
+			if(configPath == null || configPath.isEmpty()) {
+				configPath = "./src/test/resources/config.properties";
+			}
+			Properties prop = Utils.loadProperties(configPath);
+
+			String projectId = prop.getProperty("PROJECT_ID");
+			String text = DetectDocumentText.performDocumentOCR(projectId, prop.getProperty("BUCKET_NAME"), filePath);
+			log.debug("Extracted file information: \n {}", text);
+			log.info("Number of characters in extracted text: {}", text.length());
+			
+		
+		} catch (ParseException e) {
+			log.error("Parsing failed.  Reason: {}", e.getMessage());
+			formatter.printHelp("Document translator: {}", options);
+			System.exit(1);
+		} catch (NoSuchFileException e) {
+			log.error("Cannot load resource {}, {}", e.getMessage(), e);
+			System.exit(1);
+		} catch (Exception e) {
+			log.error("Error occured: {}, {}", e.getMessage(), e);
+			System.exit(1);
+		}		
+
+	}
 
 	/**
 	 * Performs document text OCR with PDF/TIFF as source files on Google Cloud
--- src/test/java/com/translator/DocumentTranslatorTest.java
+++ src/test/java/com/translator/DocumentTranslatorTest.java
@@ -5,21 +5,27 @@ import org.junit.jupiter.api.Test;
 class DocumentTranslatorTest {
 
 	@Test
-	void trasnlatetoFRtest() {
+	void translatetoFRtest() {
 		String[] args = {"-filePath", "./src/test/resources/docs/top7-trends-mule-soft.pdf", "-targetLanguage", "fr"} ;
 		DocumentTranslator.main(args);
 	}
 	
 	@Test
-	void trasnlatetoEStest() {
+	void translatetoEStest() {
 		String[] args = {"-filePath", "./src/test/resources/docs/top7-trends-mule-soft.pdf", "-targetLanguage", "es"} ;
 		DocumentTranslator.main(args);
 	}
 	
 	
 	@Test
-	void trasnlatetoRUtest() {
+	void translatetoRUtest() {
 		String[] args = {"-fDilePath", "./src/test/resources/docs/top7-trends-mule-soft.pdf", "-targetLanguage", "ru"} ;
+		DocumentTranslator.main(args);
+	}
+	
+	@Test
+	void translateNHSdoctoEStest() {
+		String[] args = {"-filePath", "./src/test/resources/docs/ENG-VIR-NHS-GUIDANCE.pdf", "-targetLanguage", "es"} ;
 		DocumentTranslator.main(args);
 	}
 
